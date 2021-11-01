b380eb011d417fbbedf05caea2ff2c849c7f5ae2
Krishna Lodha <47075664+krishnaglodha@users.noreply.github.com>
2021-11-01 17:39:18 +0530

    Update countries.sh
    User can pass countries from Makefile

--- docker-imposm3/countries.sh
+++ docker-imposm3/countries.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Can be an array, e.g.: COUNTRIES=('great-britain' 'italy')
-COUNTRIES=('great-britain')
+COUNTRIES=("$@")
 
 for country in "${COUNTRIES[@]}"
 do
-  curl -LO http://download.geofabrik.de/europe/${country}-latest.osm.pbf
+    curl -LO http://download.geofabrik.de/europe/${country}-latest.osm.pbf
 done
