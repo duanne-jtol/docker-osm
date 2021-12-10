68034d520a3c7a7859c7fe976d4c04064adcb5ba
duanne-jtol <94067254+duanne-jtol@users.noreply.github.com>
2021-12-10 09:38:54 +0000

    Update countries.sh
    add ireland and northen ireland

--- docker-imposm3/countries.sh
+++ docker-imposm3/countries.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Can be an array, e.g.: COUNTRIES=('great-britain' 'italy')
-COUNTRIES=('great-britain')
+COUNTRIES=('great-britain' 'ireland-and-northern-ireland')
 
 for country in "${COUNTRIES[@]}"
 do
   curl -LO http://download.geofabrik.de/europe/${country}-latest.osm.pbf
 done
+
